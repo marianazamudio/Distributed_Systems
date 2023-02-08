f116d215fe3b3efffadfbbf553af4c52e9d217e5
AbdelCital <105085130+AbdelCital@users.noreply.github.com>
2023-02-08 00:40:43 -0700

    Actualización
    TCP final final finalisimo

--- Chat TCP/clienteTCP.py
+++ Chat TCP/clienteTCP.py
@@ -2,7 +2,7 @@
 import threading
 import socket
 # definir un alias
-alias = input('Elige un alias >>> ')
+alias = input('Ingresa tu alias: ')
 # crear el objeto cliente
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # conectar el cliente a la direccion local y a un puerto disponible
@@ -10,36 +10,40 @@ client.connect(('127.0.0.1', 65123))
 
 # funcion para recibir mensajes de otros clientes a traves del servidor
 def client_receive():
-    while True:
-        try:
-            # encode para mandar, decode para recibir
-            message = client.recv(1024).decode('utf-8')
-            if message == "alias?":
-                # mandar el alias
-                client.send(alias.encode('utf-8'))
-            else:
-                # recibir cualquier mensaje del servidor
-                print(message)
-        except:
-            # indicar si hay un error
-            print('Error!')
-            # cerrar la conexion
-            client.close()
-            break
+	while True:
+		try:
+			# encode para mandar, decode para recibir
+			message = client.recv(1024).decode('utf-8')
+			if message == "alias?":
+				# mandar el alias
+				client.send(alias.encode('utf-8'))
+			else:
+				# This is a message from another client
+				if(("Ahora ya estas conectado!" in message) or ("se ha conectado al chat" in message)):
+					print(message)
+					
+				else:
+					print("\t\t\t\t" + message)
+		except:
+			# indicar si hay un error
+			print('Error!')
+			# indicar si hay un error
+			client.close()
+			break
 
 # funcion para mandar mensajes de otros clientes a traves del servidor
 def client_send():
     while True:
-        # el mensaje empieza con tu alias seguido del mensaje que quieres mandar
-        message = f'{alias}: {input("")}'
+        # funcion para mandar mensajes de otros clientes a traves del servidor
+        message = f'{input("")}'
         # mandar el mensaje
         client.send(message.encode('utf-8'))
-
-# crear un hilo para client_receive
+        
+# mandar el mensaje
 receive_thread = threading.Thread(target=client_receive)
-# iniciar el hilo
+# mandar el mensaje
 receive_thread.start()
-# crear un hilo para client_send
+# mandar el mensaje
 send_thread = threading.Thread(target=client_send)
-# inicial el hilo
+
 send_thread.start()
