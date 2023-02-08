f204847ef412abccd672d8665dacefe228b8d935
AbdelCital <105085130+AbdelCital@users.noreply.github.com>
2023-02-08 00:42:02 -0700

    Actualización

--- Chat TCP/servidorTCP.py
+++ Chat TCP/servidorTCP.py
@@ -19,46 +19,49 @@ aliases = []
 
 # Funcion para mandar un mensaje del servidor a todos los clientes connectados
 # esta funcion va a iterar entre la lista de clientes para mandar el mensaje
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+def broadcast(message, client):
+    for c in clients:
+        if c != client:        
+            c.send(message) 
 
 # Funcion para manejar las conexiones de cada cliente
 def handle_client(client):
+    # Get the index of the client in the client list
+    index = clients.index(client)
+    # Get the alias of the client
+    alias = aliases[index].decode('utf-8')
+
     while True:
         try:
             # mensaje recibido del cliente
             # cantidad maxima de bytes que el servidor puede recibir del cliente = 1024
-            message = client.recv(1024)
-            # mandar el mensaje
-            broadcast(message)
+            message = client.recv(1024).decode('utf-8')
+            
+            # # mandar el mensaje excepto al remitente
+            for i, c in enumerate(clients):
+                if i != index:
+                    c.send(f'{alias}: {message}'.encode('utf-8'))
+
         except:
-            # en caso de un error en la conexion
-            # se identifica el cliente con el error para quitarlo de la lista de clientes
-            # index busca en la tupla un valor especifico y devuelve la posicion donde se
-            # se encontro, en este caso, donde se encontro el cliente
-            index = clients.index(client)
-            # elimimnar el cliente que devolvio index
+            # eliminar el cliente que devolvio index
             clients.remove(client)
             # cerrar la conexion con el cliente
             client.close()
-            # se identifica el alias del cliente
-            alias = aliases[index]
+            # eliminar el alias de la lista aliases
+            aliases.remove(alias)
             # La función encode con el argumento 'utf-8' codifica un objeto de tipo string
             # en formato UTF-8. UTF-8 es una codificación de caracteres que permite
             # representar una amplia gama de caracteres internacionales
             # convierte los mensajes de tipo string en bytes
             # mandamos un mensaje para indicar quien dejo el chat
-            broadcast(f'{alias} ha abandonado el chat!'.encode('utf-8'))
-            # eliminar el alias de la lista aliases
-            aliases.remove(alias)
+            broadcast(f'{alias} has left the chat!'.encode('utf-8'),client)
             break
 
-# Funcion para recibir la conexion de los clientes
+# eliminar el alias de la lista aliases
 def receive():
     while True:
         # se empieza indicando que el servidor ya esta corriendo
-        print('El servidor esta funcionando y escuchando...')
+        print('\nEl servidor esta funcionando y escuchando...')
         # hacer que el servidor acepte cualquier conexion entrante
         # se propociona el cliente y la direccion de la conexion
         # .accept se mantiene corriendo en la espera de una conexion nueva, y regresa un
@@ -69,15 +72,19 @@ def receive():
         client.send('alias?'.encode('utf-8'))
         # guardar la respuesta del cliente
         alias = client.recv(1024)
-        # agregar el alias a la lista
+        # guardar la respuesta del cliente
         aliases.append(alias)
         # agregar el cliente a la lista
-        clients.append(client)
-        print(f'El alias de este cliente es {alias}'.encode('utf-8'))
+        clients.append(client)        
+        print(f'El alias de este cliente es "{alias.decode("utf-8")}"')
         # indicar a los demas clientes de la nueva conexion
-        broadcast(f'{alias} se ha conectado al chat'.encode('utf-8'))
-        # informarle al cliente que ya esta conectado
-        client.send('Ahora ya estas conectado!'.encode('utf-8'))
+        message = f"{alias.decode('utf-8')} se ha conectado al chat"
+        center_message = message.center(80).encode('utf-8')
+        broadcast(center_message, client)
+        # indicar a los demas clientes de la nueva conexion
+        message = "Ahora ya estas conectado!"
+        center_message = message.center(80).encode('utf-8')
+        client.send(center_message)
         # crear el hilo
         # llamar a la funcion handle_client se debe hacer a traves de un hilo
         # se debe tener un hilo por cada cliente conectado
