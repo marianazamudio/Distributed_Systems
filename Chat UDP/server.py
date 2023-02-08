e7cc98699276ae9eabb2b5622790b281e6a4b2af
marianazamudio <120000587+marianazamudio@users.noreply.github.com>
2023-02-07 21:21:18 -0700

    Update server.py
    Actualizaciones en el server

--- Chat UDP/server.py
+++ Chat UDP/server.py
@@ -41,28 +41,45 @@ def broadcast():
 			if addr not in clients:
 				clients.append(addr)
 
-			# Para no reenviar el mensaje del cliente a el mismo
-			# se inicializa al remitente y se omite el envío para éste.
-			sender = addr
+			# Checar primer elemento del header (Destinatario)
+			destinatario = message[0:20]
+			message = menssage[20:]
 
-			# Mandar el mensaje
-			for client in clients:
-				try:
-					# Omitir envío al sender
-					if client == sender:
-						continue  
-					# Si el mensaje empieza con "SIGNUP_TAG" el usuario está especificando su alias
-					if message.decode().startswith("SIGNUP_TAG:"):
-						alias = message.decode()[message.decode().index(":")+1:]
-						# Indicar que un nuevo cliente se unió al chat
-						server.sendto(f"---{alias} joined!---".encode(), client)
-					# De otro modo se envía en mesaje a todos los clientes
-					else:
-						server.sendto(message,client)
-				# Si ocurre un error al enviar el mensaje, se elimina al cliente de la lista
-				except:
-					clients.remove(client)
+			if destinatario == "all                 ":
+				# Mandar el mensaje a todos 
+				# Para no reenviar el mensaje del cliente a el mismo
+				# se inicializa al remitente y se omite el envío para éste.
+				sender = addr
+				# Registrar nuevo usuario
+				if message.decode().startswith("SIGNUP_TAG:"):
+					alias = message.decode()[message.decode().index(":")+1:]
+				for client in clients:
+					try:
+						# Omitir envío al sender
+						if client == sender:
+							continue  
+						# Si el mensaje empieza con "SIGNUP_TAG" el usuario está especificando su alias
+						if message.decode().startswith("SIGNUP_TAG:"):
+							# Indicar que un nuevo cliente se unió al chat
+							server.sendto(f"---{alias} joined!---".encode(), client)
+						# De otro modo se envía en mesaje a todos los clientes
+						else:
+							server.sendto(message,client)
+					# Si ocurre un error al enviar el mensaje, se elimina al cliente de la lista
+					except:
+						clients.remove(client)
 
+			else: 
+				# Mandar el mensaje solo a un destinatario
+				# Tomar el host y eliminar espacios
+				ip_dest = destinatario[0:15]
+				ip_dest = host_dest.replace(" ", "")
+				# Tomar el puerto y eliminar espacios
+				port_dest = destinatario[15:20]
+				port_dest = host_dest.replace(" ", "")
+				destinatario = (ip_dest,port_dest)
+				sever.sendto(message, destinatario)
+				
 # Declarar e inicializar los hilos para enviar y recibir mensajes
 t1 = threading.Thread(target=receive)
 t2 = threading.Thread(target=broadcast)
